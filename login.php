51635d9975b009a60928d010229a2c612cc17714
UdayRajSingh288 <106548320+UdayRajSingh288@users.noreply.github.com>
2024-04-28 17:53:15 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login">
+        <form action = "login.php" method = "POST">
+            <a href="./index.php">Home</a>
+            <h2>Login</h2>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <a href="./registration.php">New User/Sign Up</a>
+		<input type = "submit" name = "submit" value = "Login">
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+	session_start();
+	if (!isset($_SESSION["logged_in"])){
+		$_SESSION["logged_in"] = false;
+	}
+	if (isset($_POST["submit"])){
+		if ($_SESSION["logged_in"] == false){
+			$fp = @fopen("users.csv", "r");
+			$un = $_POST["username"];
+			$pw = hash("sha256", $_POST["password"]);
+			if ($fp){
+				while (($row = fgetcsv($fp)) != false){
+					if ($row[0] == $un && $row[2] == $pw){
+						$_SESSION["logged_in"] = true;
+						echo "<script>alert(\"You are now logged in!\");</script>";
+						break;
+					}
+				}
+				if ($_SESSION["logged_in"] == false){
+					echo "<script>alert(\"Username or password incorrect!\");</script>";
+				}
+				fclose($fp);
+			}
+			else {
+				echo "<script>alert(\"No users!!\");</script>";
+			}
+		}
+		else {
+			echo "<script>alert(\"You are already logged in!\");</script>";
+		}
+		$_POST["submit"] = null;
+	}
+?>
---
+++ registration.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./registration.css">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="regi">
+     
+        <form action = "registration.php" method = "POST">
+            <a href="./index.php">Home</a>
+            <h2>Registration</h2>
+
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <label for="password">Confirm Password:</label>
+            <input type="password" id="cpassword" name="cpassword" required>
+
+             <a href="./login.php">Already a User/Sign In</a>
+		<input type = "submit" name = "submit" value = "Register">
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+
+	function checkRecord(){
+		$fp = @fopen("users.csv", "r");
+		if ($fp){
+			while (($row = fgetcsv($fp)) != false){
+				if ($row[0] == $_POST["name"]){
+					fclose($fp);
+					return false;
+				}
+				if ($row[1] == $_POST["email"]){
+					fclose($fp);
+					return false;
+				}
+			}
+			fclose($fp);
+			return true;
+		}
+		return true;
+	}
+
+	function writeRecord(){
+		if (checkRecord()){
+			$row = array();
+			$fp = fopen("users.csv", "a");
+			$row[0] = $_POST["name"];
+			$row[1] = $_POST["email"];
+			$row[2] = hash("sha256", $_POST["password"]);
+			fputcsv($fp, $row);
+			fclose($fp);
+			return true;
+		}
+		else {
+			echo "<script>alert(\"Username or email id already taken!\");</script>";
+			return false;
+		}
+	}
+
+	if (isset($_POST["submit"])){
+		if (writeRecord()){
+			echo "<script>alert(\"Registration successful!\");</script>";
+		}
+		$_POST["submit"] = null;
+	}
+?>
